5a4396825d72febd67e4ecebcf63725a3d0a44a8
zkurtz <zkurtz@users.noreply.github.com>
2024-11-18 01:08:43 +0000

    deploy: 281ac7d601b0345cc0242e40440ee27aa2a5adac

--- packio/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Public methods and classes for packio package."""
-
-from importlib.metadata import version
-
-from packio.io import Reader as Reader
-from packio.io import Writer as Writer
-
-__version__ = version("packio")
-__all__ = ["Reader", "Writer"]
